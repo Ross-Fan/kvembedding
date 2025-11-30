16b5e793d21471684cb8eb4785993234d1660700
rexus <ross_fan@sina.com>
2025-11-30 21:40:41 +0800

    Update 11-30

--- setup.py
+++ setup.py
@@ -13,6 +13,28 @@ here = os.path.dirname(os.path.abspath(__file__))
 torch_include_dirs = cpp_extension.include_paths()
 print("::torch_include_dirs:", torch_include_dirs)
 
+# Try to find Abseil headers
+absl_include_dirs = []
+try:
+    # Try to get Abseil include path
+    result = subprocess.run(['pkg-config', '--cflags', 'absl_flat_hash_map'], 
+                          capture_output=True, text=True)
+    if result.returncode == 0:
+        absl_include_dirs = [result.stdout.strip().replace('-I', '')]
+except FileNotFoundError:
+    # pkg-config not available, try common paths
+    common_absl_paths = [
+        '/usr/local/include',
+        '/usr/include',
+        os.path.expanduser('~/abseil-cpp'),  # If manually installed in home
+    ]
+    for path in common_absl_paths:
+        absl_flat_hash_path = os.path.join(path, 'absl', 'container')
+        if os.path.exists(os.path.join(absl_flat_hash_path, 'flat_hash_map.h')):
+            absl_include_dirs = [path]
+            break
+print("::absl_include_dirs:", absl_include_dirs)
+
 ext_modules = [
     Pybind11Extension(
         "kv_table.kv_core_backend",
@@ -23,7 +45,7 @@ ext_modules = [
         include_dirs=[
             "src",
             
-        ] + torch_include_dirs,
+        ] + torch_include_dirs + absl_include_dirs,
         cxx_std=17,
         # extra_compile_args=["-O3", "-fopenmp"],
         # extra_link_args=["-fopenmp"],
